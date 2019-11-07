df59222dd1c04696ac0334ab4da1a5c52961e4c7
Joshua Laughner <jlaugh@caltech.edu>
2019-11-06 17:58:49 -0800

    New jllutils module: dataframes
    This module implements an `interpolate_to` method for dataframes/
    series and monkey-patches it onto those object. Importing it will
    automatically make those methods available on Pandas Series and
    DataFrames.

---
+++ jllutils/dataframes.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+
+def interpolate_series_to(series, values, method='linear', axis=0, limit=None, limit_direction='forward',
+                          limit_area=None, downcast=None, **kwargs):
+    new_index = series.index.union(values)
+    new_df = series.reindex(new_index).interpolate(method=method, axis=axis, limit=limit, limit_direction=limit_direction,
+                                                   limit_area=limit_area, downcast=downcast, inplace=False, **kwargs)
+    return new_df.loc[values]
+
+
+# Monkey-patch these into pandas
+pd.DataFrame.interpolate_to = interpolate_series_to
+pd.Series.interpolate_to = interpolate_series_to
