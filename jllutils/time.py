8d9e7f11ad06e1f9fd09d0f543cfd8721af74a1d
Joshua Laughner <jlaugh@caltech.edu>
2019-11-15 16:52:19 -0800

    Added start_of_month to jllutils.time

--- jllutils/time.py
+++ jllutils/time.py
@@ -1,1 +1,13 @@
 from .subutils.leapseconds import utc_to_gps, utc_to_tai, gps_to_utc, tai_to_utc, tai_to_gps, gps_to_tai, tai93_to_utc
+
+
+def start_of_month(date_in):
+    """
+    Return the start of a month as the same type as the input
+
+    :param date_in: a datetime-like object specifying a date to find the beginning of the month for. Must be a type
+     that can be constructed with a call type(date_in)(year, month, day).
+
+    :return: date of the first of the month containing ``date_in``
+    """
+    return type(date_in)(date_in.year, date_in.month, 1)
