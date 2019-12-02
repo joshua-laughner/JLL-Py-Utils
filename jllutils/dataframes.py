8c1840f3a83c22bb91657bb2902d1430c3cc821a
Joshua Laughner <jlaugh@caltech.edu>
2019-12-02 10:38:03 -0800

    dataframes.interpolate_series_to() can now handle values without a `unique()` method

--- jllutils/dataframes.py
+++ jllutils/dataframes.py
@@ -5,8 +5,15 @@ from .subutils import ncdf
 def interpolate_series_to(series, values, method='linear', axis=0, limit=None, limit_direction='forward',
                           limit_area=None, downcast=None, **kwargs):
     # input values must be unique for the index, otherwise when we do the final call to loc we can get
-    # duplicate rows
-    new_index = series.index.union(values.unique())
+    # duplicate rows. Try to use a `unique()` method on the values object if present as it often preserves
+    # types better (e.g. for Pandas DatetimeIndex objects), but if `values` does not have a `unique()`
+    # method, then fall back on `pd.unique()`.
+    try:
+        uvalues = values.unique()
+    except AttributeError:
+        uvalues = pd.unique(values)
+
+    new_index = series.index.union(uvalues)
     new_df = series.reindex(new_index).interpolate(method=method, axis=axis, limit=limit, limit_direction=limit_direction,
                                                    limit_area=limit_area, downcast=downcast, inplace=False, **kwargs)
     return new_df.loc[values]
