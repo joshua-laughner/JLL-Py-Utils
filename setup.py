4a5ba52d38b9e177917f1ee19f217996ae157b2f
Joshua Laughner <josh.laughner@jpl.nasa.gov>
2025-04-08 17:03:35 -0700

    Updated to pyproject.toml install

--- setup.py
+++ setup.py
@@ -1,13 +1,2 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='JLLUtils',
-    version='0.1',
-    packages=find_packages(),
-    install_requires=['cftime', 'matplotlib', 'netCDF4', 'numpy', 'pandas', 'pydap', 'scipy', 'statsmodels', 'xarray'],
-    url='https://github.com/joshua-laughner/JLL-Py-Utils',
-    license='',
-    author='Joshua Laughner',
-    author_email='jllacct119@gmail.com',
-    description='Collection of general Python utilities'
-)
+from setuptools import setup
+setup()
